fcd9120a05567703c9f4eae30a5ed1fc8f6408b6
shaggy2922 <shaggy2922@gmail.com>
2021-01-01 22:14:43 +0000

    v1.2.5

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from matplotlib.pyplot import figure
 import grab
 import json
 import pandas as pd
+import traceback
+
 
 print("By Evan McNab - v1.2")
 print(""" 
@@ -22,98 +24,102 @@ print("""
  
  """)
 
+def main():
 
-num = int(input("Enter number of areas to compare (4 max)>> "))
-
-metricSelector = input("Metric name (for rate per 100k enter *)>> ")
-
-if metricSelector == "*":
-    metricName = "newCasesBySpecimenDateRollingRate"
-else: 
-    metricName = metricSelector
-
-areaType = []
-areaName = []
-rawData = []
-
-for i in range(num): 
-    print(""" 
-          Enter the number of the area type {}:
-              1 United Kingdom
-              2 Nation
-              3 Region
-              4 NHS Region
-              5 Upper-tier local Authority
-              6 Lower-tier local Authority  
-              """.format(str(i)))
-              
-    areaTypeSelector = int(input(">> "))
-             
-    areaName.append(input("Enter area name (exact)>> ").upper())
-
-    if areaTypeSelector == 1:
-        areaType.append("overview")
-        areaName[i] = "UNITED KINGDOM"
-                  
-    elif areaTypeSelector == 2:
-        areaType.append("nation")
-                      
-    elif areaTypeSelector == 3:
-        areaType.append("region")
-
-    elif areaTypeSelector == 4:
-        areaType.append("nhsRegion")
+    num = 0
     
-    elif areaTypeSelector == 5:
-        areaType.append("utla")
-    
-    elif areaTypeSelector == 6:
-        areaType.append("ltla")
+    #Sets the number of areas to compare to use in for loop with exception handling
+    try:
+        num = int(input("Enter number of areas to compare (4 max)>> "))
+    except Exception as e:
+        print("""Invalid Entry - Restart
+        
+        
+        """)
+        main()
 
+    #If the number entered is too high give warning and set to maximum
+    if num >= 5:
+        print("Too many areas to compare - 4 is the maximum")
+        num = 4
+
+    #Selects the metric to graph with short cut for rate per 100k
+    metricSelector = input("Metric name (for rate per 100k enter *)>> ")
+
+    if metricSelector == "*":
+        metricName = "newCasesBySpecimenDateRollingRate"
     else: 
-        print("Invalid Entry - Restart")
-    
+        metricName = metricSelector
+
+    areaName = []
+    rawData = []
+
+    #Starts a loop to loop through the number of areas to compare
+    for i in range(num): 
+        
+        areaName.append(input("Enter area name {0}/{1} (exact)>> ".format(str(i+1),str(num))).upper())
+        
+        #Gets the data from the API handling for errors
+        try:
+            rawData = grab.grabJson(areaName[i], metricName)
+        except Exception as e:
+            print("""Invalid Entry - Restart
+            
+            
+            """)
+            main()
+        
+
+        #Formats the data to just dates and the metric
+        formattedData = pd.DataFrame(rawData['data']).sort_values(by='date')
+
+        datesOrdered = formattedData[formattedData["date"] > "2020-07-31"]
+        dates = datesOrdered[["date"]]
+
+        #Needed for plotting multiple areas on the same graph
+        if i == 0:
+            datesOrdered0 = datesOrdered
+            a1 = datesOrdered0.plot(x="date", y="metric", label = areaName[i], figsize=(10,7))
+        if i == 1:
+            datesOrdered1 = datesOrdered
+            a2 = datesOrdered1.plot(x="date", y="metric", label = areaName[i], figsize= (10,7), ax=a1)
+        if i == 2:
+            datesOrdered2 = datesOrdered
+            a3 = datesOrdered2.plot(x="date", y="metric", label = areaName[i], figsize= (10,7), ax=a2)
+        if i == 3:
+            datesOrdered3 = datesOrdered
+            a4 = datesOrdered3.plot(x="date", y="metric", label = areaName[i], figsize= (10,7), ax=a3)
 
 
-    rawData = grab.grabJson(areaType[i], areaName[i], metricName)
-    formattedData = pd.DataFrame(rawData['data']).sort_values(by='date')
+    #datetoday = str(datetime.date.today())
 
-    datesOrdered = formattedData[formattedData["date"] > "2020-07-31"]
-    dates = datesOrdered[["date"]]
+    txt = input("Enter title>> ")
 
+    #Gets the most recent date on the retrieved data
+    updatedDate = str(dates["date"].iloc[-1])
+    print(updatedDate)
 
-    if i == 0:
-        datesOrdered0 = datesOrdered
-        a1 = datesOrdered0.plot(x="date", y="metric", label = areaName[i], figsize=(10,7))
-    if i == 1:
-        datesOrdered1 = datesOrdered
-        a2 = datesOrdered1.plot(x="date", y="metric", label = areaName[i], figsize= (10,7), ax=a1)
-    if i == 2:
-        datesOrdered2 = datesOrdered
-        a3 = datesOrdered2.plot(x="date", y="metric", label = areaName[i], figsize= (10,7), ax=a2)
-    if i == 3:
-        datesOrdered3 = datesOrdered
-        a4 = datesOrdered3.plot(x="date", y="metric", label = areaName[i], figsize= (10,7), ax=a3)
+    #Makes a nice label for common metric
+    if metricName == "newCasesBySpecimenDateRollingRate":
+        formattedMetricName = "Rate per 100,000"
+    else:
+        formattedMetricName = metricName
 
 
-#datetoday = str(datetime.date.today())
-
-#txt = "Rate of COVID-19 Cases in Ipswich Per 100,000"
-txt = input("Enter title>> ")
-
-updatedDate = str(dates["date"].iloc[-1])
-print(updatedDate)
-
-if metricName == "newCasesBySpecimenDateRollingRate":
-    formattedMetricName = "Rate per 100,000"
-else:
-    formattedMetricName = metricName
+    plt.legend()
+    plt.xlabel("Date (Updated: " + updatedDate + ")", fontsize = 12)
+    plt.ylabel(formattedMetricName, fontsize = 12)
+    plt.title(txt, fontsize=20)
+    plt.show()
 
 
-plt.legend()
-plt.xlabel("Date (Updated: " + updatedDate + ")", fontsize = 12)
-plt.ylabel(formattedMetricName, fontsize = 12)
-plt.title(txt, fontsize=20)
-plt.show()
+restart = "Y"
+
+while restart == "Y":
+    restart = ""
+    main()
+    restart = input("Plot another graph (Y/N)?>>").upper()
+
+
 
 
